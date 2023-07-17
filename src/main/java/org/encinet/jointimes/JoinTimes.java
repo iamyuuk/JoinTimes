0f1d9579dbcc015a8411ca8eae4738a50ea7969d
iamyuuk <2704804982@qq.com>
2023-07-18 00:01:24 +0800

    First

---
+++ src/main/java/org/encinet/jointimes/JoinTimes.java
@@ -0,0 +1,57 @@
+package org.encinet.jointimes;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+public final class JoinTimes extends JavaPlugin implements Listener {
+    private FileConfiguration playerData;
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        Objects.requireNonNull(getCommand("jointimes")).setExecutor(this);
+        File configFile = new File(getDataFolder(), "playerData.yml");
+        if (!configFile.exists()) {
+            configFile.getParentFile().mkdirs();
+            saveResource("playerData.yml", false);
+        }
+        playerData = YamlConfiguration.loadConfiguration(configFile);
+    }
+
+    @Override
+    public void onDisable() {
+        try {
+            playerData.save(new File(getDataFolder(), "playerData.yml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        String name = e.getPlayer().getName();
+        if (!playerData.contains(name)) {
+            playerData.set(name, 1);
+        } else {
+            playerData.set(name, playerData.getInt(name));
+        }
+        e.getPlayer().sendMessage("你已经进入服务器 " + playerData.getInt(name) + " 次");
+    }
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+        int times = playerData.getInt(sender.getName());
+        sender.sendMessage("你进入服务器的次数为：" + times);
+        return true;
+    }
+}
